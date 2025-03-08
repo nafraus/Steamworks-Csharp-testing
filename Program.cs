645ff9ad6d7af46fa9a43af8dc760c065aa420d4
nafraus <nickolasfraus@gmail.com>
2025-03-08 02:17:07 -0500

    Pushing Code

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Steamworks;
+
+class Program
+{
+    static void Main()
+    {
+        if (!Packsize.Test())
+        {
+            Console.WriteLine("[Error] Steamworks.NET Packsize test failed.");
+            return;
+        }
+        if (!DllCheck.Test())
+        {
+            Console.WriteLine("[Error] Steamworks.NET DLL test failed.");
+            return;
+        }
+        if (!SteamAPI.Init())
+        {
+            Console.WriteLine("[Error] SteamAPI initialization failed. Make sure Steam is running.");
+            return;
+        }
+
+        Console.WriteLine("[Success] Steamworks initialized.");
+        Console.WriteLine("Logged in as: " + SteamFriends.GetPersonaName());
+    }
+}
